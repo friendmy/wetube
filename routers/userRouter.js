96b7388bc2f05fe8c90b73c8ae37fd9d8a5e7ad8
friendmy <friendmytrue@gmail.com>
2019-08-19 07:45:00 +0900

    controller

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import routes from "../routes";
+import { users, userDetail, editProfile, changePassword } from "../controllers/userController";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.users, (req,res) => users);
+userRouter.get(routes.userDetail, (req,res) => userDetail);
+userRouter.get(routes.editProfile, (req,res) => editProfile);
+userRouter.get(routes.changePassword, (req,res) => changePassword);
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,5 +2,11 @@ import express from "express";
 
 const videoRouter = express.Router();
 
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+
 export default videoRouter;
 
