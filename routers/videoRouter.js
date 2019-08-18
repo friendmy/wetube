51b1388cde7630aab4999afb09a44b20d8a08b1b
friendmy <friendmytrue@gmail.com>
2019-08-19 08:05:38 +0900

    pug

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,4 +1,6 @@
 import express from "express";
+import routes from "../routes";
+import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
