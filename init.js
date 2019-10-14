af3957d2f5e3c4ca150755e8a787db6c8fcec654
friendmy <friendmytrue@gmail.com>
2019-10-14 21:32:49 +0900

    commit

--- init.js
+++ init.js
@@ -2,15 +2,15 @@ import dotenv from "dotenv";
 import "./db";
 import app from "./app";
 
-dotenv.config();
-
 import "./models/Video";
 import "./models/Comment";
 import "./models/User";
 
+dotenv.config();
+
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`?? Listening on: http://localhost:${PORT}`);
+  console.log(`✔ Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
--- routes.js
+++ routes.js
@@ -28,49 +28,45 @@ const FACEBOOK = "/auth/facebook";
 const FACEBOOK_CALLBACK = "/auth/facebook/callback";
 
 const routes = {
-	home: HOME,
-	join: JOIN,
-	login: LOGIN,
-	logout: LOGOUT,
-	search: SEARCH,
-	users: USERS,
-	userDetail: id => {
-		if (id) {
-			return `/users/${id}`;
-		} else {
-			return USER_DETAIL;
-		}
-	},
-	editProfile: EDIT_PROFILE,
-	changePassword: CHANGE_PASSWORD,
-	videos: VIDEOS,
-	upload: UPLOAD,
-	videoDetail: id => {
-		if (id) {
-			return `/videos/${id}`;
-		} else {
-			return VIDEO_DETAIL;
-		}
-	},
-	editVideo: id => {
-		if (id) {
-			return `/videos/${id}/edit`;
-		} else {
-			return EDIT_VIDEO;
-		}
-	},
-	deleteVideo: id => {
-		if (id) {
-			return `/videos/${id}/delete`;
-		} else {
-			return DELETE_VIDEO;
-		}
-	},
-	githubCallback: GITHUB_CALLBACK,
-	facebookCallback: FACEBOOK_CALLBACK,
-	gitHub: GITHUB,
-	facebook: FACEBOOK,
-	me: ME
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: id => {
+    if (id) {
+      return `/users/${id}`;
+    }
+    return USER_DETAIL;
+  },
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: id => {
+    if (id) {
+      return `/videos/${id}`;
+    }
+    return VIDEO_DETAIL;
+  },
+  editVideo: id => {
+    if (id) {
+      return `/videos/${id}/edit`;
+    }
+    return EDIT_VIDEO;
+  },
+  deleteVideo: id => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    }
+    return DELETE_VIDEO;
+  },
+  githubCallback: GITHUB_CALLBACK,
+  facebookCallback: FACEBOOK_CALLBACK,
+  gitHub: GITHUB,
+  facebook: FACEBOOK,
+  me: ME
 };
 
 export default routes;
