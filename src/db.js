e7085e613557447dc892043d5bd6ca286748f6f0
friendmy <friendmy@gmail.com>
2019-10-23 13:19:27 +0900

    Mongoose

--- src/db.js
+++ src/db.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.createConnection(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useFindAndModify: false
 });
 
 const db = mongoose.connection;
